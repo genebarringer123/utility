40bf5cdbaf3f40cb8b736c6e926ece312c904b28
genebarringer123 <54601798+genebarringer123@users.noreply.github.com>
2020-02-03 12:50:21 -0600

    added files for the initial load
    added files manually

---
+++ html2xls/src/main/java/com/techdata/html2xls/HTML2XLS.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.techdata.html2xls;
+
+import com.techdata.html2xls.model.LogLines;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.apache.logging.log4j.Level;
+//import java.util.logging.Level;
+//import java.util.logging.Level;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author Gene Barringer
+ */
+public class HTML2XLS {
+
+    static Logger log = LogManager.getLogger(HTML2XLS.class);
+
+    public static void main(String[] args) {
+        Validate.isTrue(args.length == 2, "usage: supply url to fetch");
+        String html_file = args[0];
+        String xls_file = args[1];
+
+        System.out.println(LogManager.getContext().getClass());
+//        Logger.getRootLogger().setLevel(Level.WARN);
+//        HashMap<String, Level> logLevels = new HashMap<>();
+//        logLevels.put("ALL", Level.ALL);
+//        logLevels.put("TRACE", Level.TRACE);
+//        logLevels.put("DEBUG", Level.DEBUG);
+//        logLevels.put("INFO", Level.INFO);
+//        logLevels.put("WARN", Level.WARN);
+//        logLevels.put("ERROR", Level.ERROR);
+//        logLevels.put("FATAL", Level.FATAL);
+//        logLevels.put("OFF", Level.OFF);
+//        for (String name : System.getProperties().stringPropertyNames()) {
+//            String logger = "log4j.logger.";
+//            if (name.startsWith(logger)) {
+//                String loggerName = name.substring(logger.length());
+//                String loggerValue = System.getProperty(name);
+//                if (logLevels.containsKey(loggerValue)) {
+//                    Logger.getLogger(loggerName).setLevel(logLevels.get(loggerValue));
+//                } else {
+//                    Logger.getRootLogger().warn("unknown log4j logg level on comand line: " + loggerValue);
+//                }
+//            }
+//        }
+//        for (String name : System.getProperties().stringPropertyNames()) {
+//            System.out.println("name: " + name + " : " + System.getProperty(name));
+//        }
+        log.info("{}", () -> "main:  start");
+        log.info("gene testing");
+        //Turn the html file into just the text parts of the log
+        ArrayList<LogLines> al = convertHTML2ArrayList(html_file);
+        // next step is to send the arraylist to get turned in to an xls file
+        createXLS(xls_file, al);
+        log.info("main:  end");
+    }
+
+    private static ArrayList<LogLines> convertHTML2ArrayList(String html_file) {
+        log.info("convertHTML2ArrayList:  start");
+        ArrayList<LogLines> html_text = new ArrayList<LogLines>();
+        File input = new File(html_file);
+        Document doc = null;
+        try {
+            doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+        } catch (IOException ex) {
+            log.error(ex.getLocalizedMessage());
+        }
+
+        // The third table element has the actual log lines in it.
+        Element table = doc.select("table").get(3);
+        //Get all the rows in the table
+        Elements rows = table.select("tr");
+
+        log.debug("{}", () -> "convertHTML2ArrayList: number of lines: " + rows.size());
+
+        //Work through each row and grab the cell data 
+        //add each cell to the LogLines object
+        for (int i = 0; i < rows.size(); i++) {
+            Element row = rows.get(i);
+            Elements cols = row.select("td");
+
+            LogLines logLines = new LogLines();
+            logLines.setDB_date_time(cols.get(0).text());
+            logLines.setALM_Node_date_time(cols.get(1).text());
+            logLines.setThread(cols.get(2).text());
+            logLines.setRequest(cols.get(3).text());
+            logLines.setLogin(cols.get(4).text());
+            logLines.setIp(cols.get(5).text());
+            logLines.setLevel(cols.get(6).text());
+            logLines.setMethod(cols.get(7).text());
+            logLines.setMessage(cols.get(8).text());
+
+            html_text.add(logLines);
+        }
+
+        log.debug("{}", () -> "convertHTML2ArrayList - number of entries in html_text is " + html_text.size());
+//            LogLines.prettyPrint(html_text);
+
+        log.info("convertHTML2ArrayList:  end");
+        return html_text;
+    }
+
+    private static void createXLS(String xls_file, ArrayList<LogLines> al) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("logfile");
+
+        int rowCount = 0;
+        for (LogLines ll : al) {
+
+            Row row = sheet.createRow(++rowCount);
+
+            int columnCount = 0;
+            row.createCell(++columnCount).setCellValue(ll.getALM_Node_date_time());
+            row.createCell(++columnCount).setCellValue(ll.getDB_date_time());
+            row.createCell(++columnCount).setCellValue(ll.getThread());
+            row.createCell(++columnCount).setCellValue(ll.getRequest());
+            row.createCell(++columnCount).setCellValue(ll.getLogin());
+            row.createCell(++columnCount).setCellValue(ll.getIp());
+            row.createCell(++columnCount).setCellValue(ll.getLevel());
+            row.createCell(++columnCount).setCellValue(ll.getMethod());
+            row.createCell(++columnCount).setCellValue(ll.getMessage());
+        }
+
+        // Write the file to filesystem.
+        try (FileOutputStream outputStream = new FileOutputStream(xls_file)) {
+            workbook.write(outputStream);
+        } catch (FileNotFoundException ex) {
+            log.error(ex.getLocalizedMessage());
+        } catch (IOException ex) {
+            log.error(ex.getLocalizedMessage());
+        }
+    }
+
+}
---
+++ html2xls/src/main/java/com/techdata/html2xls/model/LogLines.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.techdata.html2xls.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author CBARRING
+ */
+public class LogLines implements Serializable {
+    
+    static Logger log = LogManager.getLogger(LogLines.class);
+
+    private String sampleProperty;
+    private String DB_date_time = null;
+    private String ALM_Node_date_time = null;
+    private String thread = null;
+    private String request = null;
+    private String login = null;
+    private String ip = null;
+    private String level = null;
+    private String method = null;
+    private String message = null;
+
+    public String getSampleProperty() {
+        return sampleProperty;
+    }
+
+    public void setSampleProperty(String sampleProperty) {
+        this.sampleProperty = sampleProperty;
+    }
+
+    public String getDB_date_time() {
+        return DB_date_time;
+    }
+
+    public void setDB_date_time(String DB_date_time) {
+        this.DB_date_time = DB_date_time;
+    }
+
+    public String getALM_Node_date_time() {
+        return ALM_Node_date_time;
+    }
+
+    public void setALM_Node_date_time(String ALM_Node_date_time) {
+        this.ALM_Node_date_time = ALM_Node_date_time;
+    }
+
+    public String getThread() {
+        return thread;
+    }
+
+    public void setThread(String thread) {
+        this.thread = thread;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getDB_date_time());
+        sb.append("\t");
+        sb.append(this.getALM_Node_date_time());
+        sb.append("\t");
+        sb.append(this.getThread());
+        sb.append("\t");
+        sb.append(this.getRequest());
+        sb.append("\t");
+        sb.append(this.getLogin());
+        sb.append("\t");
+        sb.append(this.getIp());
+        sb.append("\t");
+        sb.append(this.getLevel());
+        sb.append("\t");
+        sb.append(this.getMethod());
+        sb.append("\t");
+        sb.append(this.getMessage());
+        sb.append("\n");
+
+        return sb.toString();
+    }
+    
+    public static void prettyPrint(ArrayList lines) {
+        log.info("prettyPrint:  start");
+        log.debug("prettyPrint - number of entries in lines is " + lines.size());
+        for (int i = 0; i < lines.size(); i++) {
+            log.debug("prettyPrint - " + lines.get(i).toString());
+//            System.out.println(lines.get(i).toString());
+        }
+        log.info("prettyPrint:  end");
+    }
+}
